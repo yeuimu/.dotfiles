8079f33649003681802a62d29c0b5f1594b83aa1
yeuimu <2197651308@qq.com>
2024-01-24 17:54:25 +0800

    update

--- .emacs.d/lisp/init-org-ui.el
+++ .emacs.d/lisp/init-org-ui.el
@@ -1,4 +1,3 @@
-
 (setq org-hide-emphasis-markers t)
 
 (font-lock-add-keywords 'org-mode
@@ -14,5 +13,18 @@
 (setq
     org-superstar-headline-bullets-list '("⁖" "◉" "○" "✸" "✿")
 )
+
+
+(custom-theme-set-faces
+  'user
+  `(org-level-8 ((t (:inherit outline-8 ))))
+  `(org-level-7 ((t (:inherit outline-7 ))))
+  `(org-level-6 ((t (:inherit outline-6 ))))
+  `(org-level-5 ((t (:inherit outline-5 ))))
+  `(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+  `(org-level-3 ((t (:inherit outline-3 :height 1.25))))
+  `(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+  `(org-level-1 ((t (:inherit outline-1 :height 1.75))))
+  `(org-document-title ((t ( :height 2.0 :underline nil)))))
 
 (provide 'init-org-ui)
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -117,11 +117,11 @@ export QT_IM_MODULE=fcitx
 export DefaultIMModule=fcitx
 
 # system_proxy
-# export proxy="http://127.0.0.1:7890"
-# export http_proxy=$proxy
-# export https_proxy=$proxy
-# export ftp_proxy=$proxy
-# export no_proxy="localhost, 127.0.0.1, ::1"
+export proxy="http://127.0.0.1:7890"
+export http_proxy=$proxy
+export https_proxy=$proxy
+export ftp_proxy=$proxy
+export no_proxy="localhost, 127.0.0.1, ::1"
 
 export PATH=$PATH:~/.local/bin
 
