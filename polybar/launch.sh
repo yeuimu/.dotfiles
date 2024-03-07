c0f4cbf0152adeb640b79a02fd1e86b8ea5c12c7
yeuimu <2197651308@qq.com>
2024-03-07 17:28:26 +0800

    ➕improve: Adapted to the monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,14 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+
+case $1 in
+  "monitor")
+    polybar monitor 2>&1 | tee -a /tmp/polybar.log & disown
+    ;;
+  "note")
+    polybar note 2>&1 | tee -a /tmp/polybar.log & disown
+    ;;
+esac
 
 echo "Polybar launched..."
