08e26d37377459c646a34511d10dc46db1e7bf0b
yeuimu <2197651308@qq.com>
2024-03-09 00:32:37 +0800

    ➕improve: Increase text size

--- .emacs.d/lisp/custom.el
+++ .emacs.d/lisp/custom.el
@@ -12,11 +12,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-document-title ((t (:height 2.0 :underline nil))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.75))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.25))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ '(org-level-1 ((t (:inherit outline-1 :height 3))))
+ '(org-level-2 ((t (:inherit outline-2 :height 2))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.75))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.5))))
  '(org-level-5 ((t (:inherit outline-5))))
  '(org-level-6 ((t (:inherit outline-6))))
  '(org-level-7 ((t (:inherit outline-7))))
  '(org-level-8 ((t (:inherit outline-8)))))
+
+(set-face-attribute 'default nil :height 150)
