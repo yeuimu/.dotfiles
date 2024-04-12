ba05716c88e6071d3c9a85c3c4e87d47076a6aa3
yoyoki <yeuimuyeuimu@gmail.com>
2024-04-12 10:38:18 +0800

    🐛fix

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -22,21 +22,6 @@ return {
         require('mason-lspconfig').setup(opts)
       end,
     },
-    {
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'L3MON4D3/LuaSnip',
-      },
-      config = function()
-        local opts = require('configs.cmp')
-        require('cmp').setup(opts)
-      end,
-    },
   },
   opts = {
     server = {
