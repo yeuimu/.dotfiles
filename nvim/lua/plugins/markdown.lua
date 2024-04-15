e8c0b193b7d603ad0b6a10f1ca942d7298332d9c
yoyoki <yeuimuyeuimu@gmail.com>
2024-04-15 22:48:50 +0800

    ✨feat: Support Markdown After refactored

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,83 @@
+local treesitter = require('configs.comman').treesitter
+table.insert(treesitter.ft, 'markdown')
+table.insert(treesitter.opts.ensure_installed, 'markdown')
+
+return {
+  {
+    'lukas-reineke/headlines.nvim',
+    priotiy = 1000,
+    ft = { 'markdown' },
+    config = function()
+      require('headlines').setup({
+        markdown = {
+          query = vim.treesitter.query.parse(
+            'markdown',
+            [[
+                (atx_heading [
+                    (atx_h1_marker)
+                    (atx_h2_marker)
+                    (atx_h3_marker)
+                    (atx_h4_marker)
+                    (atx_h5_marker)
+                    (atx_h6_marker)
+                ] @headline)
+
+                (thematic_break) @dash
+
+                (fenced_code_block) @codeblock
+
+                (block_quote_marker) @quote
+                (block_quote (paragraph (inline (block_continuation) @quote)))
+                (block_quote (paragraph (block_continuation) @quote))
+                (block_quote (block_continuation) @quote)
+            ]]
+          ),
+          headline_highlights = { 'Headline' },
+          codeblock_highlight = 'CodeBlock',
+          dash_highlight = 'Dash',
+          dash_string = '-',
+          quote_highlight = 'Quote',
+          quote_string = '┃',
+          fat_headlines = true,
+          fat_headline_upper_string = '▃',
+          fat_headline_lower_string = '🬂',
+        },
+        rmd = {
+          query = vim.treesitter.query.parse(
+            'markdown',
+            [[
+                (atx_heading [
+                    (atx_h1_marker)
+                    (atx_h2_marker)
+                    (atx_h3_marker)
+                    (atx_h4_marker)
+                    (atx_h5_marker)
+                    (atx_h6_marker)
+                ] @headline)
+
+                (thematic_break) @dash
+
+                (fenced_code_block) @codeblock
+
+                (block_quote_marker) @quote
+                (block_quote (paragraph (inline (block_continuation) @quote)))
+                (block_quote (paragraph (block_continuation) @quote))
+                (block_quote (block_continuation) @quote)
+            ]]
+          ),
+          treesitter_language = 'markdown',
+          headline_highlights = { 'Headline' },
+          codeblock_highlight = 'CodeBlock',
+          dash_highlight = 'Dash',
+          dash_string = '-',
+          quote_highlight = 'Quote',
+          quote_string = '┃',
+          fat_headlines = true,
+          fat_headline_upper_string = '',
+          fat_headline_lower_string = '',
+        },
+      })
+    end,
+  },
+  treesitter,
+}
